fc9fb9e9a53c671d2d28b89f709453e794e7122d
roowdy <47495921+focusess@users.noreply.github.com>
2020-11-27 09:27:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace oop_lb2
+{
+
+    class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        public string Index { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Address address = new Address();
+
+            address.Country = "Ukraine";
+            address.City = "Pyriatyn";
+            address.Street = "Evropeyskaya Street";
+            address.House = "66";
+            address.Apartment = "44";
+            address.Index = "37000";
+
+            Console.WriteLine("Country: " + address.Country); 
+            Console.WriteLine("City: " + address.City);
+            Console.WriteLine("Street: " + address.Street);
+            Console.WriteLine("House: " + address.House);
+            Console.WriteLine("Apartment: " + address.Apartment);
+            Console.WriteLine("Index: " + address.Index);
+
+            Console.ReadKey();
+        }
+    }
+}
